a6b3201af111c2cd3a11d5500497dfcb398f29b0
Sandeep kumar <90573359+Sandeep4512@users.noreply.github.com>
2025-01-17 06:47:54 +0000

    datatpyes done

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,31 @@
+// data type of js
+
+"use strict"  //  treat js as newer version
+
+//alert("hello"); //  we are using nodejs not for the browser
+// ======================================================================================
+// // primity datatypes
+
+// number => 2 power of 53 
+// String   => "" & ''
+// undefine => do not assign a value
+// Boolean  => true/false
+// symbols => unique
+// null  => standalone 
+
+// user define datatypes 
+//  1. array
+// 2. Array
+
+const age =22
+const name= "Sandeep"
+let marigeDAte;    // undefine
+const Ismariged = false
+const password="Sande234@76&"
+
+
+
+
+console.log(typeof undefined)
+console.log(typeof null)
+
