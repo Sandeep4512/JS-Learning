d94495252a50e8a1ec5ac0eb86e912dd0b92849d
Sandeep kumar <90573359+Sandeep4512@users.noreply.github.com>
2025-01-17 11:26:03 +0000

    math and num

---
+++ 01_basic/05_num_math.js
@@ -0,0 +1,32 @@
+// const score = 3450;
+
+// const balance = new Number (2330)
+// console.log(balance.toString().length)
+// console.log(balance)
+// console.log(balance.toFixed(1)) // point ke kitene zero hone chahiye
+
+// const num = 23345.984
+// console.log(num.toPrecision(6))
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString("en-IN"))
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++  Maths  +++++++++++++++++++++++++++++++++++++++
+  
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.floor(4.56))
+// console.log(Math.round(3.7987))
+// console.log(Math.ceil(4.2))  // isme ek point bhi jyada honga 5 return karega 
+// console.log(Math.min(4.7))
+
+
+
+// console.log(Math.random())
+
+// console.log( Math.floor((Math.random()*10)+1))
+
+const max=20;
+const min = 10;
+console.log( Math.floor((Math.random()*(max-min+1)) +min))
