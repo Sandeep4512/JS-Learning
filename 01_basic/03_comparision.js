74aa30eef475bb52488e4ff2dd88216d9f75faf7
Sandeep kumar <90573359+Sandeep4512@users.noreply.github.com>
2025-01-17 08:41:24 +0000

    camparision done

---
+++ 01_basic/03_comparision.js
@@ -0,0 +1,14 @@
+ 
+ // ===
+
+//  console.log("22"===22) // compare to value & datatype
+
+// console.log("34"==34) //  check for values
+
+console.log(3>6)
+console.log(7>=6)
+console.log(3<6)
+console.log(4<=6)
+console.log(3==3)
+
+
