3a8c3d62ff5e6e44e919fc43d03897cc6a2fbd4c
Sandeep kumar <90573359+Sandeep4512@users.noreply.github.com>
2025-01-17 12:36:10 +0000

    date & time

---
+++ 01_basic/06_date.js
@@ -0,0 +1,24 @@
+// dates
+
+const mydate = new Date()
+// console.log(mydate.toDateString())    //Fri Jan 17 2025
+// console.log(mydate.toLocaleString())   //  1/17/2025, 12:11:25 PM
+// console.log(mydate.toISOString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toTimeString())
+// console.log(mydate.toLocaleTimeString())
+// console.log(typeof mydate)
+// const mycreatedate = new Date(2023 ,0,23)
+// // console.log(mycreatedate.toDateString())
+// console.log(mycreatedate.toLocaleString())
+
+
+// let mytime= Date.now()
+// console.log(mytime)
+
+let newdate = new Date()
+console.log(newdate)
+console.log(newdate.getMonth() +1)
+console.log(newdate.getFullYear())
+
+
