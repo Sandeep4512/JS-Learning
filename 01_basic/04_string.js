aa9946b73d0bd5f1bcb251f140bfcd6c7e9ff9ed
Sandeep kumar <90573359+Sandeep4512@users.noreply.github.com>
2025-01-17 10:32:03 +0000

    String done

--- 01_basic/04_string.js
+++ 01_basic/04_string.js
@@ -4,5 +4,74 @@
 // console.log(templateLiteralString);
 
 // method 
+// let str = "Hello, World!";
+// console.log(str.length); // Output: 13
+
+
+// let str = "hello";
+// console.log(str.toUpperCase()); // Output: HELLO
+
+// let str = "HELLO";
+// console.log(str.toLowerCase()); // Output: hello
+
+// let str = "Hello";
+// console.log(str.charAt(1)); // Output: e
+
+
+/*
+5. indexOf(substring)
+Returns the index of the first occurrence of a specified substring. Returns -1 if not found.
+
+
 let str = "Hello, World!";
-console.log(str.length); // Output: 13
+console.log(str.indexOf("World")); // Output: 7
+
+*/
+
+
+/*
+
+includes(substring)
+Checks if the string contains a specified substring. Returns true or false.
+
+
+let str = "JavaScript is fun!";
+console.log(str.includes("fun")); // Output: true
+
+*/
+
+
+
+/*
+substring(start, end)
+Extracts a portion of the string between the start and end indices (end is not included).
+
+
+let str = "Hello, World!";
+console.log(str.substring(7, 12)); // Output: World
+
+*/
+
+
+// split(delimiter)
+// Splits the string into an array of substrings using the specified delimiter.
+
+
+let str = "apple,banana,orange";
+console.log(str.split(",")); // ['apple','banana','orange']
+
+// replace(oldSubstring, newSubstring)
+// Replaces the first occurrence of a substring with another substring.
+
+
+// let str = "Hello, World!";
+// console.log(str.replace("World", "JavaScript")); // Output: Hello, JavaScript!
+
+
+// trim()
+// Removes whitespace from both ends of the string.
+
+
+// let str = "   Hello, World!   ";
+// console.log(str.trim()); // Output: Hello, World!
+
