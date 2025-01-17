318b948426b9e3759a25a8b812d715ce40eafb36
Sandeep kumar <90573359+Sandeep4512@users.noreply.github.com>
2025-01-17 07:16:55 +0000

    Conversion done

---
+++ 01_basic/02_conversion.js
@@ -0,0 +1,27 @@
+let score = 45
+
+let Convert = Number(score)
+
+// console.log(typeof score)  // direct 
+// console.log(typeof(score))   // method ways
+// console.log(Convert)
+
+
+
+
+// '45' => 33
+// "3324" => NaN
+// true => 1 false => 0
+
+// let Islogined = ""
+// let convertedvalue = Boolean(Islogined)
+// console.log(convertedvalue)
+
+// 1-> true
+//0-> false
+// "Sandeep" -> true
+
+let someNumer = 34
+let StringNumer = String(someNumer)
+console.log(StringNumer)
+console.log( typeof StringNumer)
