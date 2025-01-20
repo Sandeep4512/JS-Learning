c68bbc75d484397b9189e89f33179a2afbab2561
Sandeep kumar <90573359+Sandeep4512@users.noreply.github.com>
2025-01-20 19:15:08 +0000

    Array done

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -3,7 +3,7 @@
 // [] ->  bracket
 // {} ->  curlibrases
 
- const myArray = [ 0,4,9,3,4,5]  // resizeing its not a fixed
+//  const myArray = [ 0,4,9,3,4,5]  // resizeing its not a fixed
 
 // myArray.push(5)   -> add  last index 
 // myArray.push(6)
@@ -12,14 +12,38 @@
 
 // myArray.shift()   -> remove zero index
 
-  console.log(myArray.includes(5))  // ->return  Boolean  value True / False
-  console.log(myArray.indexOf(9))    // ->  
+//   myArray.includes(5)  // ->return  Boolean  value True / False
+//   myArray.indexOf(9)   // ->  return of position
   
   
 //   const  newArray = myArray.slice(0 , 4)  // range dena hota hai but leave the  last value the range original array do not chnaged 
   
-console.log(myArray)
-  const new1 = myArray.splice(0 , 4)   //-> original array are chanaged
+// console.log(myArray)
+//   const new1 = myArray.splice(0 , 4)   //-> original array are chanaged
 
-console.log(new1)
-console.log(myArray)
+// console.log(new1)
+// console.log(myArray)
+
+
+
+const hero = ["Sandeep","Rohit","raju","Rohan","Shubham"]
+const bollybood = ["Ajay","spiderman","shaktiman"]
+
+//  bollybood.push(hero);  // return of length of Array 
+// console.log(bollybood)   
+ const newArray1 = bollybood.concat(hero) // return of new Array 
+// console.log(newArray1)
+
+const spredArry= [...bollybood,...hero,"more","than","one"] // spred operator
+// console.log(spredArry)
+
+// IsArray 
+// from
+// of
+
+const array3 = "Sandeep"
+console.log(Array.isArray(array3)) //   return boolean 
+console.log(Array.from(array3)) // every char to convert index form like this  [ 'S', 'a', 'n','d', 'e', 'e','p' ]
+console.log(Array.of(array3))  
+ //  direct convert into string to Array from like ['Sandeep']
+ // may be Number or String 
