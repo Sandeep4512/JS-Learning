1c6c6e5fe600df82785bc838607c20fd9ecdd237
Sandeep kumar <90573359+Sandeep4512@users.noreply.github.com>
2025-01-29 19:07:52 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,22 @@
+// let a=10
+// const b=20
+// var c =40
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// -----------------------------------
+// let a = 100
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     var c = 40
+//     console.log("inner ", a)
+// }
+
+//  console.log(a)
+// console.log(b)
+// console.log(c)
+
+// ******************************************************
