aacf2779779f7f6498af4a534d5d558f7ea0bb86
Sandeep kumar <90573359+Sandeep4512@users.noreply.github.com>
2025-01-29 19:11:53 +0000

    scope 2

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -6,6 +6,10 @@
 // console.log(b)
 // console.log(c)
 
+//   {} -> scope : blcok level scope  inside the body
+//                : global level scope outsite the body 
+     
+
 // -----------------------------------
 // let a = 100
 // if (true) {
